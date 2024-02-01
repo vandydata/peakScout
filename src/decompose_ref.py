36ec4978adc301996e5624e8abf385410ea30f22
allx2100 <allx2100@gmail.com>
2024-01-31 22:38:34 -0600

    added other genomes

--- src/decompose_ref.py
+++ src/decompose_ref.py
@@ -2,6 +2,9 @@ import pandas as pd
 import os
 
 def decompose_df(ref_dir, species, full_ref, col_names):
+    if not os.path.exists(ref_dir + species):
+        os.mkdir(ref_dir + species)
+
     full_df = pd.read_csv(ref_dir + species + "/" + full_ref, sep = "\t", skiprows=5, names = col_names)
 
     decomposed_dfs = {name: group for name, group in full_df.groupby(['chr', 'type'], group_keys=False)}
@@ -15,22 +18,17 @@ def decompose_df(ref_dir, species, full_ref, col_names):
         group.to_csv(ref_dir + species + "/" + type_name + "/" + chr)
 
 ref_dir = 'reference/'
-mm10_ref = 'gencode.vM25.annotation.gtf'
 col_names = ['chr', 'source', 'type', 'start', 'end', 'ph1', 'strand', 'ph2', 'info']
 
+mm10_ref = 'gencode.vM25.annotation.gtf'
 decompose_df(ref_dir, "mm10", mm10_ref, col_names)
 
-# mm10_df = pd.read_csv(ref_dir + mm10_ref, sep = '\t', skiprows=5, names=col_names)
+mm39_ref = 'gencode.vM34.annotation.gtf'
+decompose_df(ref_dir, "mm39", mm39_ref, col_names)
 
-# decomposed_dfs = {(chr, type_name): group for (chr, type_name), group in mm10_df.groupby(['chr', 'type'], group_keys=False)}
+hg19_ref = 'gencode.v19.annotation.gtf'
+decompose_df(ref_dir, "hg19", hg19_ref, col_names)
 
-# for name, group in decomposed_dfs.items():
-#     chr = name[0]
-#     type_name = name[1]
-#     if not os.path.exists(ref_dir + type_name):
-#         os.mkdir(ref_dir + type_name)
-    
-#     group.to_csv(ref_dir + type_name + "/" + chr)
-
-        
+hg38_ref = 'gencode.v45.annotation.gtf'
+decompose_df(ref_dir, "hg38", hg38_ref, col_names)
 
