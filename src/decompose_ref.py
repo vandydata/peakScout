915d8e656c50f444d77961a3e9e4a4c6d2853e4e
allx2100 <allx2100@gmail.com>
2024-02-12 21:01:32 -0600

    generate end csvs

--- src/decompose_ref.py
+++ src/decompose_ref.py
@@ -7,16 +7,29 @@ def decompose_df(ref_dir, species, full_ref, col_names):
 
     full_df = pd.read_csv(ref_dir + species + "/" + full_ref, sep = "\t", skiprows=5, names = col_names)
 
-    decomposed_dfs = {name: group.sort_values(by = 'start') for name, group in full_df.groupby(['chr', 'type'], group_keys=False)}
+    decomposed_dfs_start = {name: group.sort_values(by = 'start') for name, group in full_df.groupby(['chr', 'type'], group_keys=False)}
+    decomposed_dfs_end = {name: group.sort_values(by = 'end') for name, group in full_df.groupby(['chr', 'type'], group_keys=False)}
 
-    for name, group in decomposed_dfs.items():
+    for name, group in decomposed_dfs_start.items():
         chr = name[0]
         type_name = name[1]
         if not os.path.exists(ref_dir + species + "/" + type_name):
             os.mkdir(ref_dir + species + "/" + type_name)
 
         group = split_jumble(group)
-        group.to_csv(ref_dir + species + "/" + type_name + "/" + chr + '.csv') 
+        group.drop_duplicates(subset='start', keep='first', inplace=True)
+        group.to_csv(ref_dir + species + "/" + type_name + "/" + chr + '-start.csv') 
+        print(name)
+
+    for name, group in decomposed_dfs_end.items():
+        chr = name[0]
+        type_name = name[1]
+        if not os.path.exists(ref_dir + species + "/" + type_name):
+            os.mkdir(ref_dir + species + "/" + type_name)
+
+        group = split_jumble(group)
+        group.drop_duplicates(subset='start', keep='first', inplace=True)
+        group.to_csv(ref_dir + species + "/" + type_name + "/" + chr + '_end.csv') 
         print(name)
 
 def split_jumble(df):
@@ -48,12 +61,12 @@ col_names = ['chr', 'source', 'type', 'start', 'end', 'ph1', 'strand', 'ph2', 'i
 mm10_ref = 'gencode.vM25.annotation.gtf'
 decompose_df(ref_dir, "mm10", mm10_ref, col_names)
 
-mm39_ref = 'gencode.vM34.annotation.gtf'
-decompose_df(ref_dir, "mm39", mm39_ref, col_names)
+# mm39_ref = 'gencode.vM34.annotation.gtf'
+# decompose_df(ref_dir, "mm39", mm39_ref, col_names)
 
-hg19_ref = 'gencode.v19.annotation.gtf'
-decompose_df(ref_dir, "hg19", hg19_ref, col_names)
+# hg19_ref = 'gencode.v19.annotation.gtf'
+# decompose_df(ref_dir, "hg19", hg19_ref, col_names)
 
-hg38_ref = 'gencode.v45.annotation.gtf'
-decompose_df(ref_dir, "hg38", hg38_ref, col_names)
+# hg38_ref = 'gencode.v45.annotation.gtf'
+# decompose_df(ref_dir, "hg38", hg38_ref, col_names)
 
