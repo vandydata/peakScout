ef961c34427890768e24dd6a2de9ba135af62223
allx2100 <allx2100@gmail.com>
2024-04-17 12:23:39 -0500

    #3 add documentation and helper fxns

--- src/get_nearest_feature.py
+++ src/get_nearest_feature.py
@@ -1,108 +1,111 @@
 import polars as pl
+import numpy as np
 
-def get_nearest_features(roi, feature, starts, ends, up_bound, down_bound, k):
-    feature_starts = starts.select(['start','end']).to_numpy()
-    feature_ends = ends.select('end').to_numpy().flatten()
-    assert(len(feature_starts) == len(feature_ends))
+def get_nearest_features(roi: pl.DataFrame, 
+                         feature: str, 
+                         starts: pl.DataFrame, 
+                         ends: pl.DataFrame, 
+                         up_bound: int, 
+                         down_bound: int, 
+                         k: int) -> pl.DataFrame:
+    '''
+    Determine the nearest k features to each peak in roi using the reference 
+    starts and ends. 
+
+    Parameters:
+    roi (pl.DataFrame): Polars DataFrame containing peaks and relevant information.
+    feature (str): The feature of interest.
+    starts (pl.DataFrame): Polars DataFrame of reference features sorted by start position.
+    ends (pl.DataFrame): Polars DataFrame of reference features sorted by end position.
+    up_bound (int): Maximum allowed distance between peak and upstream feature.
+    down_bound (int): Maximum allowed distance between peak and downstream feature.
+    k (int): number of nearest features to collect
+
+    Returns:
+    return_roi (pl.DataFrame): Polars DataFRame containing peak information, the 
+    nearest k features to that peak, and the distances between those k features 
+    and the peak.
+
+    Outputs:
+    None
+    '''
+
+    starts_sub = starts.select(['start','end']).to_numpy()
+    ends_sub = ends.select('end').to_numpy().flatten()
+    assert(len(starts_sub) == len(ends_sub))
 
     start_features = starts.select(feature).to_numpy().flatten()
     end_features = ends.select(feature).to_numpy().flatten()
 
-    return_roi = None
-    if 'name' in roi.columns:
-        return_roi = roi.select(['name', 'chr', 'start', 'end']).clone()
-    else:
-        return_roi = roi.select(['chr', 'start', 'end']).clone()
+    return_roi = roi.select(['name', 'chr', 'start', 'end']).clone()
 
     index = 0
-    features_to_add = {}
-    dists_to_add = {}
-    for i in range(1, k+1):
-        features_to_add[i] = []
-        dists_to_add[i] = []
+    
+    overlap_features = []
+    overlap_index = 0
+
+    features_to_add, dists_to_add = gen_init(k)
 
     for peak in return_roi.iter_rows(named=True):
         peak_start = peak['start']
         peak_end = peak['end']
-
-        downstream_start, downstream_bound, upstream_bound, upstream_end = constrain_features(feature_starts, feature_ends, 
-                                                                                              down_bound, up_bound,
-                                                                                              peak_start, peak_end)
         
-        # print(downstream_bound, upstream_bound)
-        constrained_feature_starts = feature_starts[downstream_start : downstream_bound]
-        constrained_feature_ends = feature_ends[upstream_bound : upstream_end]
+        c_starts_sub, c_ends_sub, c_start_features, c_end_features = constrain_features(peak_start, peak_end,
+                                                                                        starts_sub, ends_sub,
+                                                                                        start_features, end_features, 
+                                                                                        up_bound, down_bound)
 
-        constrained_starts = start_features[downstream_start : downstream_bound]
-        constrained_ends = end_features[upstream_bound : upstream_end]
+        ds_index = 0
+        us_index = len(c_end_features) - 1
 
-        downstream_index = 0 #constrained_starts.searchsorted(peak_start, side='left')
-        upstream_index = len(constrained_ends) - 1 #constrained_ends.searchsorted(peak_end, side='right') - 1
+        assert(len(c_start_features) == len(c_starts_sub))
+        assert(len(c_end_features) == len (c_ends_sub))
 
-        assert(len(constrained_starts) == len(constrained_feature_starts))
-        assert(len(constrained_ends) == len (constrained_feature_ends))
-
-        overlap_genes = []
-        overlap_index = 0
-        while len(constrained_feature_starts) > overlap_index and constrained_feature_starts[overlap_index][0] <= peak_end:
-            if constrained_feature_starts[overlap_index][0] <= peak_start  \
-                and constrained_feature_starts[overlap_index][1] >= peak_start:
-                overlap_genes.append(overlap_index)
-            elif constrained_feature_starts[overlap_index][0] <= peak_end  \
-                and constrained_feature_starts[overlap_index][1] >= peak_end:
-                overlap_genes.append(overlap_index)
-            overlap_index += 1
+        overlap_features, overlap_index = find_overlaps(peak_start, peak_end, c_starts_sub, 
+                                                     overlap_features, overlap_index)
         
         i = k
-        zero_index = 0
+        overlap_ctr = 0
 
-        while(zero_index < len(overlap_genes) and i > 0):
-            features_to_add[k-i+1].append(constrained_starts[overlap_genes[zero_index]])
-            dists_to_add[k-i+1].append(str(0))
-            zero_index += 1
+        while(overlap_ctr < len(overlap_features) and i > 0):
+            update_to_add(features_to_add, dists_to_add, c_start_features, 0, k-i+1, overlap_features[overlap_ctr])
+            overlap_ctr += 1
             i -= 1
 
-        while i > 0 and upstream_index > -1 and downstream_index < len(constrained_starts):
-            downstream_dist = constrained_feature_starts[downstream_index][0] - peak_end
-            upstream_dist = peak_start - constrained_feature_ends[upstream_index]
+        while i > 0 and us_index > -1 and ds_index < len(c_start_features):
+            ds_dist = max(0, c_starts_sub[ds_index][0] - peak_end)
+            us_dist = max(0, peak_start - c_ends_sub[us_index])
 
-            downstream_dist = 0 if downstream_dist < 0 else downstream_dist
-            upstream_dist = 0 if upstream_dist < 0 else upstream_dist
-
-            if downstream_dist == 0:
-                downstream_index += 1
+            if ds_dist == 0:
+                ds_index += 1
                 continue
             
-            if upstream_dist == 0:
-                upstream_index -= 1
+            if us_dist == 0:
+                us_index -= 1
                 continue
 
-            if downstream_dist < upstream_dist:
-                features_to_add[k-i+1].append(constrained_starts[downstream_index])
-                dists_to_add[k-i+1].append(str(downstream_dist))
-                downstream_index += 1
+            if ds_dist < us_dist:
+                update_to_add(features_to_add, dists_to_add, c_start_features, ds_dist, k-i+1, ds_index)
+                ds_index += 1
             else:
-                features_to_add[k-i+1].append(constrained_ends[upstream_index])
-                dists_to_add[k-i+1].append(str(-1 * upstream_dist))
-                upstream_index -= 1
+                update_to_add(features_to_add, dists_to_add, c_end_features, -1 * us_dist, k-i+1, us_index)
+                us_index -= 1
         
             i -= 1
         
-        if i > 0 and upstream_index < 0:
-            while i > 0 and downstream_index < len(constrained_starts):
-                features_to_add[k-i+1].append(constrained_starts[downstream_index])
-                downstream_dist = constrained_feature_starts[downstream_index][0] - peak_end
-                downstream_dist = 0 if downstream_dist < 0 else downstream_dist
-                dists_to_add[k-i+1].append(str(downstream_dist))
-                downstream_index += 1
+        if i > 0 and us_index < 0:
+            while i > 0 and ds_index < len(c_start_features):
+                ds_dist = c_starts_sub[ds_index][0] - peak_end
+                ds_dist = max(0, ds_dist)
+                update_to_add(features_to_add, dists_to_add, c_start_features, ds_dist, k-i+1, ds_index)
+                ds_index += 1
                 i -= 1
-        elif i > 0 and downstream_index >= len(constrained_starts):
-            while i > 0 and upstream_index > -1:
-                features_to_add[k-i+1].append(constrained_ends[upstream_index])
-                upstream_dist = peak_start - constrained_feature_ends[upstream_index]
-                upstream_dist = 0 if upstream_dist < 0 else upstream_dist
-                dists_to_add[k-i+1].append(str(-1 * upstream_dist))
-                upstream_index -= 1
+        elif i > 0 and ds_index >= len(c_start_features):
+            while i > 0 and us_index > -1:
+                us_dist = peak_start - c_ends_sub[us_index]
+                us_dist = max(0, us_dist)
+                update_to_add(features_to_add, dists_to_add, c_end_features, -1 * us_dist, k-i+1, us_index)
+                us_index -= 1
                 i -= 1
         
         while i > 0:
@@ -112,6 +115,143 @@ def get_nearest_features(roi, feature, starts, ends, up_bound, down_bound, k):
 
         index += 1
     
+    return gen_return_roi(return_roi, feature, features_to_add, dists_to_add, k)
+
+def constrain_features(peak_start: int, 
+                       peak_end: int, 
+                       starts: np.ndarray, 
+                       ends: np.ndarray, 
+                       start_features: np.ndarray, 
+                       end_features: np.ndarray, 
+                       up_bound: int, 
+                       down_bound: int) -> tuple[np.ndarray, np.ndarray,
+                                                 np.ndarray, np.ndarray]:
+    '''
+    Constrain the reference start and ends features contain only valid features -- 
+    those within down_bound/up_bound distance of the peak and those on one 
+    particular side of the peak.
+
+    Parameters:
+    peak_start (int): Start position of peak.
+    peak_end (int): End position of peak.
+    starts (np.ndarray): NumPy array of start positions of reference features.
+    ends (np.ndarray): NumPy array of end positions of reference features.
+    start_features (np.ndarray): NumPy array of reference features sorted by start position.
+    end_features (np.ndarray): NumPy array of reference features sorted by end position.
+    up_bound (int): Maximum allowed distance between peak and upstream feature.
+    down_bound (int): Maximum allowed distance between peak and downstream feature.
+
+    Returns:
+    c_starts (np.ndarray): NumPy array containing constrained start positions of reference features.
+    c_ends (np.ndarray): NumPy array containing constrained end positions of reference features.
+    c_start_features (np.ndarray): NumPy array containing constrained reference features sorted by start position.
+    c_end_features (np.ndarray): NumPy array containing constrained reference features sorted by end position.
+
+    Outputs:
+    None
+    '''
+    if down_bound is not None:
+        ds_upper = starts.searchsorted(peak_end + down_bound, side='right')
+    else:
+        ds_upper = len(starts)
+    
+    if up_bound is not None:
+        us_lower = ends.searchsorted(peak_start - up_bound, side='left')
+    else:
+        us_lower = 0
+    
+    ds_lower = 0
+    us_upper = ends.searchsorted(peak_end, side='right')
+
+    c_starts = starts[ds_lower : ds_upper]
+    c_ends = ends[us_lower : us_upper]
+
+    c_start_features = start_features[ds_lower : ds_upper]
+    c_end_features = end_features[us_lower : us_upper]
+
+    return c_starts, c_ends, c_start_features, c_end_features
+
+def check_overlap(peak_start: int, 
+                  peak_end: int, 
+                  starts: np.ndarray, 
+                  index: int) -> bool:
+    '''
+    Determine if the feature at the given index overlaps with the peak.
+
+    Parameters:
+    peak_start (int): Start position of peak.
+    peak_end (int): End position of peak.
+    starts (np.ndarray): NumPy array of start positions of reference features.
+    index (int): Index of the feature in question.
+
+    Returns:
+    True if the feature overlaps the peak; false otherwise.
+
+    Outputs:
+    None
+    '''
+    return (starts[index][0] <= peak_start and starts[index][1] >= peak_start) or \
+            (starts[index][0] <= peak_end and starts[index][1] >= peak_end)
+
+def find_overlaps(peak_start: int, 
+                  peak_end: int, 
+                  starts: np.ndarray, 
+                  overlap_features: list, 
+                  overlap_index: int) -> tuple[list, int]:
+    '''
+    Find the indicies of genes that overlap with the peak.
+
+    Parameters:
+    peak_start (int): Start position of peak.
+    peak_end (int): End position of peak.
+    starts (np.ndarray): NumPy array of start positions of reference features.
+    overlap_features (list): A list of overlapping features from the previous peak. If this
+                             is the first peak, this list is empty. 
+    overlap_index (int): The index of the first feature in starts that begins after the previous peak ends.
+                         If this is the first peak, this integer is 0. 
+
+    Returns:
+    overlap_features (list): A list of overlapping features for the given peak.
+    overlap_index (int): Index of the first feeature in starts that begins after the current peak ends.
+    
+    Outputs:
+    None
+    '''
+    for gene in overlap_features:
+            if not check_overlap(peak_start, peak_end, starts, overlap_index):
+                overlap_features.remove(gene)
+
+    while len(starts) > overlap_index and starts[overlap_index][0] <= peak_end:
+        if check_overlap(peak_start, peak_end, starts, overlap_index):
+            overlap_features.append(overlap_index)
+        overlap_index += 1
+    
+    return overlap_features, overlap_index
+
+def gen_return_roi(return_roi: pl.DataFrame,
+                   feature: str,
+                   features_to_add: dict,
+                   dists_to_add: dict, 
+                   k: int) -> pl.DataFrame:
+    '''
+    Generates Polars DataFrame containing peak information, the nearest k features to that peak, 
+    and the distances between those k features and the peak.
+
+    Parameters:
+    return_roi (pl.DataFrame): Skeleton for return Polars DataFrame with all necessary columns.
+    feature (str): Feature in question.
+    features_to_add (dict): Dicitionary that maps integer n with a list of the nth closest feature.
+    dists_to_add (dict): Dictionary that maps integer n with a list of the distance between the peak
+                         and the nth closest feature.
+    k (int): Number of closest features to determine.
+
+    Returns:
+    return_roi (pl.DataFrame): Polars DataFrame containing peak information, the nearest k features to that peak, 
+    and the distances between those k features and the peak.
+
+    Outputs:
+    None
+    '''
     for i in range(1, k+1):
         if feature == 'gene_name':
             return_roi = return_roi.with_columns([pl.Series('closest_' + feature + '_' + str(i), features_to_add[i]),
@@ -119,21 +259,54 @@ def get_nearest_features(roi, feature, starts, ends, up_bound, down_bound, k):
         else: 
             return_roi = return_roi.with_columns([pl.Series('closest_' + feature + '_' + str(i), features_to_add[i]),
                                               pl.Series('closest_' + feature + '_' + str(i) + '_dist', dists_to_add[i])])
-
     return return_roi
 
-def constrain_features(feature_starts, feature_ends, down_bound, up_bound, peak_start, peak_end):
-    if down_bound is not None:
-        downstream_bound = feature_starts.searchsorted(peak_end + down_bound, side='right')
-    else:
-        downstream_bound = len(feature_starts)
-    
-    if up_bound is not None:
-        upstream_bound = feature_ends.searchsorted(peak_start - up_bound, side='left')
-    else:
-        upstream_bound = 0
-    
-    downstream_start = 0
-    upstream_end = feature_ends.searchsorted(peak_end, side='right')
+def gen_init(k: int) -> tuple[dict, dict]:
+    '''
+    Generates dictionaries to add to Polars DataFrame skeletons for nearest feature information.
 
-    return downstream_start, downstream_bound, upstream_bound, upstream_end
+    Parameters:
+    k (int): Number of nearest features to determine.
+
+    Returns:
+    features_to_add (dict): Dicitionary that maps integer n with a list of the nth closest feature.
+    dists_to_add (dict): Dictionary that maps integer n with a list of the distance between the peak
+                         and the nth closest feature.
+
+    Outputs:
+    None
+    '''
+    features_to_add = {}
+    dists_to_add = {}
+    for i in range(1, k+1):
+        features_to_add[i] = []
+        dists_to_add[i] = []
+    
+    return features_to_add, dists_to_add
+
+def update_to_add(add_features: dict,
+                  add_dists: dict,
+                  features: np.ndarray, 
+                  dist: int, 
+                  add_index: int, 
+                  feature_index: int) -> None:
+    '''
+    Adds a feature and its distance from a peak to given dictionaries.
+
+    Parameters:
+    add_features (dict): Dicitionary that maps integer n with a list of the nth closest feature.
+    add_dists (dict): Dictionary that maps integer n with a list of the distance between the peak
+                         and the nth closest feature.
+    features (np.ndarray): NumPy array containing list of features.
+    dist (int): Distance to add to dictionary.
+    add_index (int): Index of peak for updating.
+    feature_index (int): Index of feature.
+
+    Returns:
+    None
+
+    Outputs:
+    Updates add_features and add_dists to contain the newest feature and dist.
+    '''
+    add_features[add_index].append(features[feature_index])
+    add_dists[add_index].append(str(dist))
