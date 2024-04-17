3390faf5aa3b214e4bfe12d022f10218b759be95
allx2100 <allx2100@gmail.com>
2024-04-17 12:56:54 -0500

    #2 changed working dirs

--- test/test_MACS2.sh
+++ test/test_MACS2.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-python3 ../src/peakScout.py peak2gene \
-    --peak_file ../test/test_MACS2.bed \
+python3 src/peakScout.py peak2gene \
+    --peak_file test/test_MACS2.bed \
     --peak_type MACS2 \
     --species test \
     --k 3 \
-    --ref_dir test-reference \
+    --ref_dir test/test-reference \
     --output_name test_MACS2 \
-    --o results \
+    --o test/results \
     --output_type csv
 
-python3 compare_csv.py \
-    --a ../test/results/test_MACS2.csv \
-    --e ../test/test_MACS2_expected_results.csv
+python3 test/compare_csv.py \
+    --a test/results/test_MACS2.csv \
+    --e test/test_MACS2_expected_results.csv
