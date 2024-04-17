513039ecaa7bd37821c1f025f3d3e0d99a73c27d
allx2100 <allx2100@gmail.com>
2024-04-17 13:23:08 -0500

    #2 add new dependency

--- test/remove_previous.sh
+++ test/remove_previous.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-if test -f test_MACS2.csv; then
-    rm test_MACS2.csv
-fi
-
-if test -f test_SEACR.csv; then
-    rm test_SEACR.csv
+if test -d results; then
+    rm -rf results
 fi
--- test/test_SEACR.sh
+++ test/test_SEACR.sh
@@ -7,9 +7,9 @@ python3 src/peakScout.py peak2gene \
     --k 3 \
     --ref_dir test/test-reference \
     --output_name test_SEACR \
-    --o test/ \
+    --o test/results/ \
     --output_type csv
 
 python3 test/compare_csv.py \
-    --a test/test_SEACR.csv \
+    --a test/results/test_SEACR.csv \
     --e test/test_SEACR_expected_results.csv
