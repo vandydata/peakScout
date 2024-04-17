7578b20247abba3d1f5e5eec8c80510dca6249ca
allx2100 <allx2100@gmail.com>
2024-04-17 15:19:11 -0500

    #2 fixed test_decomp

--- test/test_decomp.sh
+++ test/test_decomp.sh
@@ -2,7 +2,7 @@
 
 python3 src/peakScout.py decompose \
     --species test \
-    --ref_dir reference/ \
-    --gtf_ref reference/test/test.gtf \
-    --o reference/test \
+    --ref_dir test/test-reference/ \
+    --gtf_ref test/test-reference/test.gtf \
+    --o test/test-reference/test \
 
