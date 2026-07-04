3380dc5d71f32f666f94867e1289973254867064
JP Cartailler <jp.cartailler@vanderbilt.edu>
2026-07-04 11:22:57 -0500

    new test for cCRE and end of line fixes

--- test/test_peak2gene_MACS2_cre.sh
+++ test/test_peak2gene_MACS2_cre.sh
@@ -6,10 +6,10 @@ peakScout peak2gene \
     --species_genome mm39 \
     --k 3 \
     --ref_dir test/test-reference/test \
+    --use_cre \
     --output_name test_peak2gene_MACS2_cre \
     --o test/results/ \
-    --output_type csv \
-    --cre_file test/test_cre.bed
+    --output_type csv
 
 python3 test/compare_csv.py \
     --a test/results/test_peak2gene_MACS2_cre.csv \
